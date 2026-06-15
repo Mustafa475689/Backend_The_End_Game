7f1d8dbae5559ef82de40bc4384cd94b7d93b028
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-15 12:45:09 +0500

    endgame
    endgame

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,44 @@ const userModel = require('./users');
 
 /* GET home page. */
 router.get('/', function (req, res) {
+  req.session.ban = true; // use session // session ki huta ha? session huta ha serer pe data.
+  res.cookie("age", 21); // set cookie // cookie ka data browser pe set hut ha.
   res.render('index');
 });
+
+// read cookie
+router.get('/readcookie', function (req, res) {
+  console.log(req.cookies.age);
+  res.send("check");
+}); 
+
+router.get('/deletecookie', function (req, res) {
+  res.clearCookie('age');
+  res.send("delete cookie")
+});
+
+
+
+// session /.................
+router.get('/checkban', function (req, res) {
+  if (req.session.ban === true) { // check session
+    res.send("you are banned");
+  }
+  else {
+    res.send("not banned");
+  }
+
+});
+
+router.get('/removeban', function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) throw err;
+    res.send("ban removed");
+  });
+
+});
+//////////
+
 
 // Now we start learn create, read update and delete:
 
@@ -20,18 +56,18 @@ router.get('/create', async function (req, res) {
   //
 });
 
-  // Find user
-  router.get('/allusers', async function (req, res) {
-    // let allusers = await userModel.find(); /// to find all users
-    let allusers = await userModel.findOne({username: "Mustafa24"}); /// to find any specific user by his user name...
-    res.send(allusers);
-  });
+// Find user
+router.get('/allusers', async function (req, res) {
+  // let allusers = await userModel.find(); /// to find all users
+  let allusers = await userModel.findOne({ username: "Mustafa24" }); /// to find any specific user by his user name...
+  res.send(allusers);
+});
 
-  // Delete users
-  router.get('/delete', async function (req, res) {
-    let deleteduser = await userModel.findOneAndDelete({username: "Mustafa24"}); /// to find any specific user by his user name...
-    res.send(deleteduser);
-  });
+// Delete users
+router.get('/delete', async function (req, res) {
+  let deleteduser = await userModel.findOneAndDelete({ username: "Mustafa24" }); /// to find any specific user by his user name...
+  res.send(deleteduser);
+});
 
 
 module.exports = router; 
